0df14c8617f96c60f9381047d740c579efcf5cc6
jakub <kiszczyckuba20@wp.pl>
2024-11-28 13:53:50 +0100

    Feat: Added small tweak to neo tree

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -4,17 +4,10 @@ local config = require "neo-tree"
 vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem right<CR>", { noremap = true, silent = true })
 
 config.setup {
-  hide_hidden = false,
-  hide_dotfiles = false,
-  hide_gitignored = false,
   close_if_last_window = false,
   popup_border_style = "rounded",
   enable_git_status = true,
   enable_diagnostics = true,
-follow_current_file = {
-            enabled = true,
-            leave_dirs_open = false,
-          },
   file_size = {
     enabled = true,
     required_width = 5,
@@ -33,6 +26,15 @@ follow_current_file = {
     enabled = false,
   },
   filesystem = {
+    filtered_items = {
+      hide_hidden = false,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+    },
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = false,
+    },
     window = {
       mappings = {
         ["<BS>"] = "close_node",

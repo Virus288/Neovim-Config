61f7885ad3484298064dea5931e71a132fd03462
jakub <kiszczyckuba20@wp.pl>
2024-11-29 21:35:03 +0100

    Feat: Added missing go to definition buttons

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("i", "jk", "<ESC>")
 map("n", "<leader>dd", vim.lsp.buf.code_action, { desc = "Under cursor actions" })
 
 map("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file" })
+map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementaion" })
+map("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })

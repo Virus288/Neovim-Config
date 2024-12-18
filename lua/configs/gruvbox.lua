a33105f8d3a252e71b5f4ac3c74e35b6d48da97f
Jakub <kiszczyckuba20@wp.pl>
2024-12-18 20:45:47 +0100

    Feat: Added gruvbox theme

---
+++ lua/configs/gruvbox.lua
@@ -0,0 +1,26 @@
+local config = require "gruvbox"
+
+config.setup {
+  terminal_colors = true,
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = true,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true,
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+}

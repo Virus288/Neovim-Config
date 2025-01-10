33ed70fce8deda6221f0cf682c11b64b026a9e8c
Jakub <kiszczyckuba20@wp.pl>
2025-01-10 08:11:00 +0100

    Fix: Reverted last commit. Replacing yanking commands, using OSC 52 just broke copying data

--- lua/keyOptions.lua
+++ lua/keyOptions.lua
@@ -12,19 +12,6 @@ vim.keymap.set("n", ";", ":", { silent = true, desc = "CMD enter command mode" }
 vim.keymap.set("v", ";", ":", { silent = true, desc = "CMD enter command mode" })
 vim.keymap.set("i", "jk", "<ESC>", { silent = true, desc = "Edit edit mode" })
 
--- Add support for ssh clipboard
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  -- paste = {
-  --   ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-  --   ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  -- },
-}
-
 -- Yank to system clipboard
 vim.keymap.set('n', 'y', '"+y', { noremap = true, silent = true })
 vim.keymap.set('v', 'y', '"+y', { noremap = true, silent = true })

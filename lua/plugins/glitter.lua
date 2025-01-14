9d120a09788d745016d4b54d289b385d81c0e0e3
Jakub <kiszczyckuba20@wp.pl>
2025-01-14 07:20:51 +0100

    Feat: Added glitter plugin to animate some movements

---
+++ lua/plugins/glitter.lua
@@ -0,0 +1,14 @@
+return {
+    "rachartier/tiny-glimmer.nvim",
+    event = "TextYankPost",
+    opts = {
+      enabled = true,
+      default_animation = "pulse",
+          pulse = {
+            max_duration = 1000,
+            chars_for_max_duration = 15,
+            pulse_count = 2,
+            intensity = 7,
+        },
+    },
+}

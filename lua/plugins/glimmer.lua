81166c68825cb15b1298bcb3e7ea9755fe187deb
Jakub <kiszczyckuba20@wp.pl>
2025-01-27 07:46:12 +0100

    Fix: Removed duplicated code

--- lua/plugins/glimmer.lua
+++ lua/plugins/glimmer.lua
@@ -2,12 +2,13 @@ return {
     "rachartier/tiny-glimmer.nvim",
     event = "TextYankPost",
     opts = {
-        default_animation = "left_to_right",
-        pulse = {
+      enabled = true,
+      default_animation = "pulse",
+          pulse = {
             max_duration = 1000,
             chars_for_max_duration = 15,
             pulse_count = 2,
-            intensity = 1,
+            intensity = 7,
         },
     },
 }

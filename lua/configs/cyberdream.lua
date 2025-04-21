f998bc40f4a144138b5d4bb624c7c9d823549578
jakub <kiszczyckuba20@wp.pl>
2025-04-21 14:09:59 +0200

    Feat: Added new theme

---
+++ lua/configs/cyberdream.lua
@@ -0,0 +1,15 @@
+require("cyberdream").setup({
+    variant = "light",
+    transparent = false,
+    saturation = 1,
+    italic_comments = true,
+    hide_fillchars = true,
+    borderless_pickers = true,
+    terminal_colors = true,
+    cache = false,
+    extensions = {
+        telescope = true,
+        notify = true,
+        mini = true,
+    },
+})

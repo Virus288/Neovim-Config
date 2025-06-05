f33c959808b70bed5a7f9aece1b6f387ae9c4f5c
Jakub <kiszczyckuba20@wp.pl>
2025-06-05 09:15:12 +0200

    Feat: Added few small plugins

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/trouble.nvim",
+  opts = {},
+
+  cmd = "Trouble",
+
+  keys = {
+    {
+      "<leader>dt",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  },
+}
